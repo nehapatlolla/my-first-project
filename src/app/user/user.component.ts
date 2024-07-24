65526d41f6b360789959336093c3a6f44c128aad
nehapatlolla <neha.patlolla@proclink.com>
2024-07-24 14:32:25 +0530

    movie-list component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  
    @Input() name = " Neha"
+   @Input() img = "https://packandbianesvision.com/wp-content/uploads/2022/06/girl-smiling-min-1024x672.jpg"
 }
 
 

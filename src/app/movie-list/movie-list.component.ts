a7d07a2916224f424eaefdabbc51a61afc88392e
nehapatlolla <neha.patlolla@proclink.com>
2024-07-25 12:09:32 +0530

    Added movie finally

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,13 +1,30 @@
 import { Component, Input } from '@angular/core';
 
 import { MoviedataComponent } from '../moviedata/moviedata.component';
+import { FormsModule } from '@angular/forms';
+import { IMovie } from '../app.component';
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [MoviedataComponent],
+  imports: [MoviedataComponent, FormsModule],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
 export class MovieListComponent {
-  @Input() movies: any;
+  @Input() movies: Array<IMovie> = [];
+
+  name = '';
+  poster = '';
+  rating = '';
+  summary = '';
+
+  addMovie() {
+    let latestMovie: IMovie = {
+      name: this.name,
+      poster: this.poster,
+      rating: +this.rating,
+      summary: this.summary,
+    };
+    this.movies.push(latestMovie);
+  }
 }

b10a2c8725dc70ff40ed73ed736460ebfec20c34
nehapatlolla <neha.patlolla@proclink.com>
2024-07-26 11:19:40 +0530

    everuthing works

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -3,15 +3,115 @@ import { Component, Input } from '@angular/core';
 import { MoviedataComponent } from '../moviedata/moviedata.component';
 import { FormsModule } from '@angular/forms';
 import { IMovie } from '../app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [MoviedataComponent, FormsModule],
+  imports: [
+    MoviedataComponent,
+    FormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
 export class MovieListComponent {
-  @Input() movies: Array<IMovie> = [];
+  // @Input() movies: Array<IMovie> = [];
+  movies: Array<IMovie> = [
+    {
+      name: 'Vikram',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg',
+      rating: 8.4,
+      summary:
+        'Members of a black ops team must track and eliminate a gang of masked murderers.',
+    },
+    {
+      name: 'RRR',
+      poster: 'https://assemble.imgix.net/3462/poster.jpg?w=920',
+      rating: 8.8,
+      summary:
+        'RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.',
+    },
+    {
+      name: 'Iron man 2',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg',
+      rating: 7,
+      summary:
+        'With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.',
+    },
+    {
+      name: 'No Country for Old Men',
+      poster:
+        'https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg',
+      rating: 8.1,
+      summary:
+        "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    },
+    {
+      name: 'Jai Bhim',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg',
+      summary:
+        'A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case',
+      rating: 8.8,
+    },
+    {
+      name: 'The Avengers',
+      rating: 8,
+      summary:
+        "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
+      poster:
+        'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg',
+    },
+    {
+      name: 'Interstellar',
+      poster: 'https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg',
+      rating: 8.6,
+      summary:
+        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.',
+    },
+    {
+      name: 'Baahubali',
+      poster: 'https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg',
+      rating: 8,
+      summary:
+        'In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.',
+    },
+    {
+      name: 'Ratatouille',
+      poster:
+        'https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=',
+      rating: 8,
+      summary:
+        'Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.',
+    },
+    {
+      name: 'PS2',
+      poster:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjKtB1bEf93FszO7NbCY6zlbhiDSZc_7GHBeZl6r78a0_SbCJfqK5EKZjyx_hMOBmIz0MUOk1NNMrVeDYYtAWnW7rqbQnkGnBfQ7Vh368WS6waUi2SwolRTCsDU7X0Qe4x8BwWMreKDhykSS5XYztaGr2zk97aqFlTVAaC9G6sajaqS6cLdQJbuCUm8mA/w1200-h630-p-k-no-nu/Ponniyin%20Selvan-2-PS-2-Movie-Box-Office-Hit-or-Flop.jpg',
+      summary:
+        'Ponniyin Selvan: I is an upcoming Indian Tamil-language epic period action film directed by Mani Ratnam, who co-wrote it with Elango Kumaravel and B. Jeyamohan',
+      rating: 8,
+    },
+    {
+      name: 'Thor: Ragnarok',
+      poster:
+        'https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg',
+      summary:
+        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.',
+      rating: 8.8,
+    },
+  ];
 
   name = '';
   poster = '';
